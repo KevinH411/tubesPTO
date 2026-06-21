22bf1a0e0853ac4ead05bc760dcbfc3fd4889db1
unknown <andrewaruan@gmail.com>
2026-06-21 16:51:07 +0700

    slight modification

--- src/poa/PelicanExploration.java
+++ src/poa/PelicanExploration.java
@@ -10,18 +10,18 @@ public class PelicanExploration {
         this.fitness = fitness;
     }
 
-    /**
-     * Menghitung posisi baru "kandidat" berdasarkan posisi pelikan saat ini
-     * (saatIni) dan posisi mangsa (mangsa), lalu menyimpan hasilnya ke
-     * dalam vektor Solution.arr milik kandidat.
-     *
-     * Catatan: kandidat, saatIni, dan mangsa harus sudah dievaluasi
-     * (sudah punya nilai makespan & totalFlowTime) sebelum method ini
-     * dipanggil, karena dipakai untuk menentukan arah pergerakan.
-     */
+    // menghitung posisi baru "kandidat" berdasarkan posisi pelikan saat ini
+    // dengan posisi mangsa
+    // lalu menyimpan hasilnya ke dalam vektor Solution.arr milik kandidat.
+
+    // catatan kandidat, saatIni, dan mangsa harus sudah dievaluasi :
+    // sudah punya nilai makespan & totalFlowTime sebelum method ini
+    // dipanggil, karena dipakai untuk menentukan arah pergerakan.
+
     public void hitung(Pelican kandidat, Pelican saatIni, Pelican mangsa, Random rand) {
         boolean mangsaLebihBaik = fitness.isLebihBaik(mangsa, saatIni);
-        int I = rand.nextInt(2) + 1; // bernilai 1 atau 2, sesuai rumus POA asli
+        int I = rand.nextInt(2) + 1; // bernilai 1 atau 2, sesuai rumus POA, 2 = berpindah lebih jauh (eksplorasi)
+                                     // dan 1 = menetap.
 
         for (int j = 0; j < kandidat.getSolusi().arr.length; j++) {
             double posisiSaatIni = saatIni.getSolusi().arr[j];
